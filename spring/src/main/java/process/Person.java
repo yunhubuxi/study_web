7fa3ccce2b5a4f72ba31be6a17ffb8c4d112b1bc
yunhubuxi <296023290@qq.com>
2019-07-30 13:29:35 +0800

    spring核心模块学习笔记

--- spring/src/main/java/process/Person.java
+++ spring/src/main/java/process/Person.java
@@ -82,15 +82,16 @@ public class Person implements BeanFactoryAware, BeanNameAware,
                 .println("【InitializingBean接口】调用InitializingBean.afterPropertiesSet()");
     }
 
-    // 这是DiposibleBean接口方法
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("【DiposibleBean接口】调用DiposibleBean.destory()");
-    }
 
     // 通过<bean>的init-method属性指定的初始化方法
     public void myInit() {
         System.out.println("【init-method】调用<bean>的init-method属性指定的初始化方法");
+    }
+
+    // 这是DiposibleBean接口方法
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("【DiposibleBean接口】调用DiposibleBean.destory()");
     }
 
     // 通过<bean>的destroy-method属性指定的初始化方法
