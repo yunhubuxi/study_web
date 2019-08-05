a47cf710bf5c928336765d25666e1e27c6b01f0c
yunhubuxi <296023290@qq.com>
2019-08-05 09:10:30 +0800

    Update MyBeanFactoryPostProcessor.java

--- spring/src/main/java/process/MyBeanFactoryPostProcessor.java
+++ spring/src/main/java/process/MyBeanFactoryPostProcessor.java
@@ -7,18 +7,17 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
 public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 
-    public MyBeanFactoryPostProcessor() {
-        super();
-        System.out.println("这是BeanFactoryPostProcessor实现类构造器！！");
-    }
+  public MyBeanFactoryPostProcessor() {
+    super();
+    System.out.println("这是BeanFactoryPostProcessor实现类构造器！！");
+  }
 
-    @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory arg0)
-            throws BeansException {
-        System.out
-                .println("【BeanFactoryPostProcessor】调用postProcessBeanFactory方法");
-        BeanDefinition bd = arg0.getBeanDefinition("person");
-        bd.getPropertyValues().addPropertyValue("phone", "110");
-    }
+  @Override
+  public void postProcessBeanFactory(ConfigurableListableBeanFactory arg0)
+      throws BeansException {
+    System.out.println("【BeanFactoryPostProcessor】调用postProcessBeanFactory方法");
+    BeanDefinition bd = arg0.getBeanDefinition("person");
+    bd.getPropertyValues().addPropertyValue("phone", "110");
+  }
 
 }
