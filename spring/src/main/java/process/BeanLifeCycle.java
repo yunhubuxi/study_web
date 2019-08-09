14f19ef99409cc8cb163b3dc485891ceb7d11489
yunhubuxi_Y400 <296023290@qq.com>
2019-08-09 20:53:21 +0800

    Update BeanLifeCycle.java

--- spring/src/main/java/process/BeanLifeCycle.java
+++ spring/src/main/java/process/BeanLifeCycle.java
@@ -1,39 +1,25 @@
 package process;
 
-
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class BeanLifeCycle {
 
     public static void main(String[] args) {
 
         System.out.println("===================现在开始初始化容器===================");
-
-
-//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("springBeanTest/beans.xml");
-        BeanFactory factory = new XmlBeanFactory(new ClassPathResource("springBeanTest/beans.xml"));
-        System.out.println("容器初始化成功");
-
-
-
-        Object beanFactoryPostProcessor= factory.getBean("beanFactoryPostProcessor");
-        Person person = (Person) factory.getBean("FactoryBeanPersonTest");
-
-        System.out.println(person);
-
-        System.out.println("现在开始关闭容器！");
-//        ((ClassPathXmlApplicationContext)factory).registerShutdownHook();
-        ApplicationContext factory = new ClassPathXmlApplicationContext("springBeanTest/beans.xml");
-
+        // BeanFactory factory = new XmlBeanFactory(new ClassPathResource("springBeanTest/beans.xml"));
+        // System.out.println("容器初始化成功");
+        // Object beanFactoryPostProcessor= factory.getBean("beanFactoryPostProcessor");
+        // Person person = (Person) factory.getBean("FactoryBeanPersonTest");
+        // System.out.println(person);
+        // System.out.println("现在开始关闭容器！");
+        // ((ClassPathXmlApplicationContext)factory).registerShutdownHook();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("springBeanTest/beans.xml");
         System.out.println("===================容器初始化成功===================");
-
-        Person person = factory.getBean("person", Person.class);
-
+        Person person = applicationContext.getBean("person", Person.class);
         System.out.println(person);
-
         System.out.println("===================现在开始关闭容器===================");
-        ((ClassPathXmlApplicationContext)factory).registerShutdownHook();
+        ((ClassPathXmlApplicationContext) applicationContext).registerShutdownHook();
     }
 }
