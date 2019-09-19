0018ee2d092a847501b359a5ced9c22dc988dcdb
yunhubuxi <296023290@qq.com>
2019-09-19 09:23:54 +0800

    dubbo 测试

--- dubbo/dubbo-demo-provider/src/main/java/provider/DemoServiceImpl.java
+++ dubbo/dubbo-demo-provider/src/main/java/provider/DemoServiceImpl.java
@@ -5,6 +5,7 @@ import api.DemoService;
 public class DemoServiceImpl implements DemoService {
     @Override
     public String sayHello(String name) {
+        name = "aaa";
         return "Hello " + name;
     }
 }
