2af9a13e2d79299ad011cad3575df829a90795ff
yunhubuxi <296023290@qq.com>
2019-09-19 16:34:04 +0800

    dubbo 测试

--- dubbo/dubbo-demo-client/src/main/java/consumer/Consumer.java
+++ dubbo/dubbo-demo-client/src/main/java/consumer/Consumer.java
@@ -1,6 +1,5 @@
 package consumer;
 
-import api.DemoService;
 import com.yonyoucloud.uretail.api.IBillQueryService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,7 +12,6 @@ public class Consumer {
         // DemoService demoService = (DemoService)context.getBean("demoService"); // 获取远程服务代理
         // String hello = demoService.sayHello("world"); // 执行远程方法
         // System.out.println( hello ); // 显示调用结果
-
         IBillQueryService billQueryService = (IBillQueryService)context.getBean("billQueryService"); // 获取远程服务代理
         billQueryService.register(new HashMap<>()); // 执行远程方法
     }
