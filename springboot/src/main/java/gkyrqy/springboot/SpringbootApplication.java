0a3b38f226323603f5371b7dc30e7b636f6da079
yunhubuxi <296023290@qq.com>
2020-02-25 11:01:51 +0800

    Update SpringbootApplication.java

--- springboot/src/main/java/gkyrqy/springboot/SpringbootApplication.java
+++ springboot/src/main/java/gkyrqy/springboot/SpringbootApplication.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 1,测试替换beandefinition，在同一个配置文件中，就不能替换，因为已经存在，无法注册
- *   如果非要替换，则把原来的移除，重新注入
+ * 如果非要替换，则把原来的移除，重新注入
  * 2,bean有个属性，可以配置是否可以被替换
  * 3,如果不在一个配置文件里，后注册的默认会替换掉先注册的
  */
@@ -27,16 +27,11 @@ public class SpringbootApplication {
         System.out.println(authorServer.getAuthor());
         DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) applicationContext.getAutowireCapableBeanFactory();
         BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(
-                gkyrqy.springboot.service_1.DemoA.class);
+            gkyrqy.springboot.service_1.DemoA.class);
         defaultListableBeanFactory.removeBeanDefinition("demoA");
         defaultListableBeanFactory.registerBeanDefinition("demoA",
-                beanDefinitionBuilder.getBeanDefinition());
-        Demo demo = (Demo)applicationContext.getBean("demoA");
+            beanDefinitionBuilder.getBeanDefinition());
+        Demo demo = (Demo) applicationContext.getBean("demoA");
         demo.sayHello();
-
-
     }
-
-
-
 }
