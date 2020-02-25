59ac2c83dd8026d5b0d3912235ad40cfb6c41026
yunhubuxi <296023290@qq.com>
2020-02-25 12:37:29 +0800

    remove uretail test

--- dubbo/dubbo-demo-client/src/main/java/consumer/Consumer.java
+++ dubbo/dubbo-demo-client/src/main/java/consumer/Consumer.java
@@ -1,34 +1,7 @@
 package consumer;
 
 import api.DemoService;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.parser.ParserConfig;
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.gson.JsonObject;
-import com.yonyoucloud.manufacturing.ed.material.Material;
-import com.yonyoucloud.manufacturing.service.material.IMaterialQueryService;
-import com.yonyoucloud.uretail.api.IBillQueryService;
-import com.yonyoucloud.uretail.bill.dto.BaseDto;
-import com.yonyoucloud.uretail.bill.dto.BillDataDto;
-import com.yonyoucloud.uretail.dao.Pager;
-import com.yonyoucloud.uretail.dao.meta.MetaDaoHelper;
-import com.yonyoucloud.uretail.model.BillContext;
-import com.yonyoucloud.uretail.ref.model.RefEntity;
-import com.yonyoucloud.uretail.util.json.GsonHelper;
-import net.sf.json.JsonConfig;
-import org.imeta.orm.base.BizObject;
-import org.imeta.orm.schema.QueryCondition;
-import org.imeta.orm.schema.QueryField;
-import org.imeta.orm.schema.QueryPager;
-import org.imeta.orm.schema.QuerySchema;
-import org.springframework.cglib.beans.BeanMap;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class Consumer {
     public static void main(String[] args) throws Exception {
@@ -36,22 +9,10 @@ public class Consumer {
         context.start();
         // 获取远程服务代理
         DemoService demoService = (DemoService) context.getBean("demoService");
-        // 执行远程方法
-        Map map = new HashMap();
-        map.put("111", "bbb");
-        map.put("222", "bbb");
-        Map ddd = demoService.sayHello(map);
         String hello = demoService.sayHello("world");
         // 显示调用结果
         System.out.println(hello);
-        test(materialQueryService);
+
     }
 
-    private static void test(IMaterialQueryService materialQueryService) throws Exception{
-        Map<String, String> stringStringMap = materialQueryService.queryMaterialById(1453393419079936L);
-        System.out.println(stringStringMap.toString());
-
-        List<Map<String, String>> mapList = materialQueryService.queryMaterialByProductId(1453714209771776L);
-        System.out.println(mapList.toString());
-    }
 }
--- dubbo/dubbo-demo-provider/src/main/java/provider/DemoServiceImpl.java
+++ dubbo/dubbo-demo-provider/src/main/java/provider/DemoServiceImpl.java
@@ -1,7 +1,6 @@
 package provider;
 
 import api.DemoService;
-import com.alibaba.fastjson.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,7 +15,6 @@ public class DemoServiceImpl implements DemoService {
     @Override
     public Map sayHello(Map map) {
         System.out.println(map.toString());
-        System.out.println(JSONObject.toJSONString(map));
         map.put("111", "sdfsdf");
         map.put("222", "sdfsdfsdfsdsdf");
         map.put("fsdf", "fsdf");
