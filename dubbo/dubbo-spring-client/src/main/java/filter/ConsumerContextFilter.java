0ea2c51651e2a11bb431c0ac23af7889b9f6ef5d
yunhubuxi_Y400 <296023290@qq.com>
2020-03-01 19:51:23 +0800

    test

--- dubbo/dubbo-spring-client/src/main/java/filter/ConsumerContextFilter.java
+++ dubbo/dubbo-spring-client/src/main/java/filter/ConsumerContextFilter.java
@@ -13,6 +13,8 @@ public class ConsumerContextFilter implements Filter {
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         String token = "bttdkl3eGV4ZXVzVGZ0NksxdktkRzVmVnFhTHE1bE9hVTVDNjNUYnBsakI4YTBiU3M1bXRCRUp2VTRjZXJjRVFIK0ZjL0lSdi8zVmxITVhhSzZvUG9pQURxMnF0NGdjQncya3BrSkVyR1FIMVU9XzAzMmQ2NTI5LTM1YjYtNDFiNi04ODE2LTE1Y2FhNDFhNTU1OF9pZHRlc3QueXl1YXAuY29tXzE1NzgwNDI4MTY4ODE.__1578376311892";
         RpcContext.getContext().setAttachment("rpcToken", token);
+        System.out.println(invoker.getUrl().toString());
+        System.out.println(invocation.toString());
         return invoker.invoke(invocation);
     }
 }
