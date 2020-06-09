7f5dbf97da2ec472c33e3b0ca1448f0a8a93a840
yunhubuxi_Y400 <296023290@qq.com>
2020-06-09 20:18:27 +0800

    Create SpringApplicationThreadLocal.java

---
+++ springboot/src/main/java/gkyrqy/springboot/SpringApplicationThreadLocal.java
@@ -0,0 +1,11 @@
+package gkyrqy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationThreadLocal {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplicationThreadLocal.class);
+    }
+}
