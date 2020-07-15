fef806c6263b85d6579158c802234b6fa2c3feca
yunhubuxi <296023290@qq.com>
2020-07-15 21:16:00 +0800

    。。。

---
+++ springboot/src/main/java/gkyrqy/springboot/service/DemoTwoImpl.java
@@ -0,0 +1,15 @@
+package gkyrqy.springboot.service;
+
+import gkyrqy.springboot.interface1.DemoTwo;
+
+public class DemoTwoImpl implements DemoTwo {
+    @Override
+    public void sayHello() {
+
+    }
+
+    @Override
+    public void sayTwoHello() {
+
+    }
+}
