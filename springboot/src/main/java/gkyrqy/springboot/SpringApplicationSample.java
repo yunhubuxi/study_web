33166daa53897c77172f7bd1e7ea5881194b0122
yunhubuxi <296023290@qq.com>
2020-07-27 09:28:26 +0800

    注入配置文件优先级、原理
    注入配置文件优先级、原理

--- springboot/src/main/java/gkyrqy/springboot/SpringApplicationSample.java
+++ springboot/src/main/java/gkyrqy/springboot/SpringApplicationSample.java
@@ -1,6 +1,9 @@
 package gkyrqy.springboot;
 
+import author.AuthorProperties;
+import author.AuthorServer;
 import gkyrqy.springboot.aop.TestBean;
+import gkyrqy.springboot.service.BaseDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,6 +14,9 @@ public class SpringApplicationSample {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringApplicationSample.class);
         System.out.println(SpringApplicationSample.class.getResource("/"));
         applicationContext.getBean(TestBean.class).test();
-
+        System.out.println(applicationContext.getBean(BaseDemo.class).getBaseTest());
+        System.out.println(applicationContext.getBean(BaseDemo.class).getBaseTestTwo());
+        System.out.println(applicationContext.getBean(AuthorServer.class).getAuthor());
+        System.out.println(applicationContext.getBean(AuthorProperties.class).toString());
     }
 }
