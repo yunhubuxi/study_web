f6fb2b7edbfb3e15c0ca4fa5ee1f9b10aa794a3c
yunhubuxi <296023290@qq.com>
2021-03-18 20:50:51 +0800

    Revert "Revert "dubbo consumer scope test""
    This reverts commit a836152af03825b03058397829adba2397dc0267.

---
+++ dubbo/dubbo-spring-client/src/main/java/consumer/DemoServiceImpl.java
@@ -0,0 +1,24 @@
+package consumer;
+
+import api.DemoService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DemoServiceImpl implements DemoService {
+    @Override
+    public String sayHello(String name) {
+        System.out.println(this.hashCode());
+        name = "aaa";
+        return "Hello consume " + name;
+    }
+
+    @Override
+    public Map sayHello(Map map) {
+        System.out.println(map.toString());
+        map.put("111", "sdfsdf");
+        map.put("222", "sdfsdfsdfsdsdf");
+        map.put("fsdf", "fsdf");
+        return new HashMap();
+    }
+}
