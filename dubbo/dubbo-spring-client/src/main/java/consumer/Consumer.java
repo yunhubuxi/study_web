fbc77ad251356e6da6cfb734b02493b28a5a09e0
yunhubuxi <296023290@qq.com>
2021-03-18 21:23:24 +0800

    Revert "Revert "Revert "dubbo consumer scope test"""
    This reverts commit f6fb2b7edbfb3e15c0ca4fa5ee1f9b10aa794a3c.

--- dubbo/dubbo-spring-client/src/main/java/consumer/Consumer.java
+++ dubbo/dubbo-spring-client/src/main/java/consumer/Consumer.java
@@ -3,21 +3,15 @@ package consumer;
 import api.DemoService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.concurrent.TimeUnit;
-
 public class Consumer {
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"consumer.xml"});
         context.start();
         // 获取远程服务代理
-        DemoService demoService = (DemoService) context.getBean("aaa");
-        for (int i = 0; i < 100; i++) {
-            TimeUnit.MILLISECONDS.sleep(100);
-            String hello = demoService.sayHello("world");
-            // 显示调用结果
-            System.out.println(hello);
-        }
-
+        DemoService demoService = (DemoService) context.getBean("demoService");
+        String hello = demoService.sayHello("world");
+        // 显示调用结果
+        System.out.println(hello);
 
     }
 
