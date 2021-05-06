03b51f9f204a3049662d919bd8a5c8a43b3d74b8
yunhubuxi <296023290@qq.com>
2021-05-06 18:57:59 +0800

    异步调用

--- dubbo/dubbo-spring-client/src/main/java/consumer/Consumer.java
+++ dubbo/dubbo-spring-client/src/main/java/consumer/Consumer.java
@@ -1,7 +1,13 @@
 package consumer;
 
 import api.DemoService;
+import com.alibaba.dubbo.rpc.Result;
+import com.alibaba.dubbo.rpc.RpcContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class Consumer {
     public static void main(String[] args) throws Exception {
@@ -9,10 +15,20 @@ public class Consumer {
         context.start();
         // 获取远程服务代理
         DemoService demoService = (DemoService) context.getBean("demoService");
+        sync(demoService);
+        async(demoService);
+    }
+
+    static void sync(DemoService demoService) {
         String hello = demoService.sayHello("world");
         // 显示调用结果
         System.out.println(hello);
+    }
 
+    static void async(DemoService demoService) throws InterruptedException, ExecutionException, TimeoutException {
+        demoService.sayHello("world");
+        Object result = RpcContext.getContext().getFuture().get(5, TimeUnit.SECONDS);
+        System.out.println(result);
     }
 
 }
