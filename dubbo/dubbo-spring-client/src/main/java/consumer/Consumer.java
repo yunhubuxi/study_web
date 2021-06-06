dda4230c56daa5e287ba5b1728e6f73aa239b6a7
yunhubuxi_Y400 <296023290@qq.com>
2021-06-06 20:34:40 +0800

    dubbo相关验证

--- dubbo/dubbo-spring-client/src/main/java/consumer/Consumer.java
+++ dubbo/dubbo-spring-client/src/main/java/consumer/Consumer.java
@@ -4,11 +4,17 @@ import api.DemoService;
 import com.alibaba.dubbo.rpc.Result;
 import com.alibaba.dubbo.rpc.RpcContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+
+/**
+ * 业务线程（如：main线程）发送数据，提交任务的任务队列，然后阻塞
+ * NettyClientWorker线程负责执行handle，然后发送数据，channel最后发送了数据，接受到数据后
+ * 提交DubboClientHandler-localhost:20880-thread线程上处理
+ * 处理完后唤醒main线程
+ */
 public class Consumer {
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"consumer.xml"});
